ecf73d973b1ed0b2bba8c939b39692f63e100185
windisiswandi <windisiswandi89@gmail.com>
2023-07-24 13:35:40 +0800

    feat:membuat_landing_page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./views/**/*.{html,ejs}"],
   theme: {
-    extend: {},
+    container: {
+      center: true
+    },
+    screens: {
+      'desktop': '1440px'
+    },
+    fontFamily: {
+      "modern": "modern-no-20-regular",
+      "jakartaSans" : "Plus Jakarta Sans",
+      "poppins" : "Poppins",
+      "inter" : "Inter"
+    },
+    extend: {
+      colors: {
+        "primary" : "#025464",
+        "secondary": "#E57C23"
+      }
+    },
   },
   plugins: [],
 }
